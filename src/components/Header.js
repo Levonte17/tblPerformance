32b7b4a22cbb4262bd8d515902f75c437684cba6
Levonte Larry <you@example.com>
2024-05-26 07:06:10 -0400

    ready to goooo/set up

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+
+function Header(props) {
+  return <h1> Trained By Lockdown </h1>;
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+
+function Header(props) {
+    return <h1> Main </h1>;
+  }
+  
+  export default Header;
+  
