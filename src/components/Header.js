22962e5111ef7170751d21ba28d07eb4508ece67
Levonte Larry <you@example.com>
2024-05-27 07:01:25 -0400

    define routes, installed links, ready to start

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
+
 
 function Header(props) {
-  return <h1> Trained By Lockdown </h1>;
+  return(
+    <nav>
+         <Link to="/">
+         <div>Trained By Lockdown Performance</div>
+         </Link>
+    </nav>
+  )
 }
 
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Index from '../pages/Index';
 import Show from '../pages/Show';
+
 function Header(props) {
-    return <h1> Main </h1>;
+    return(
+        <main>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/people/:id" element={<Show />} />
+            </Routes>
+        </main>
+    )
   }
   
   export default Header;
